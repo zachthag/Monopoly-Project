2b13c51179bf0b3c8b9e9c3d00cf7ad0e073914f
ZacharyGass <zachary.g757@gmail.com>
2026-03-05 15:36:18 -0800

    Initial Monopoly Project Setup

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+
+    std::cout <<"Monopoly Project";
+}
